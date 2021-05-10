a6b62b5aaa63a8fcfa377192c8084320e9e62c47
Dmitrii Kozikov <kozikov.dmitrii@mail.ru>
2021-05-10 10:47:04 +0400

    Fixed polynomial toString method

--- src/entities/Polynomial.java
+++ src/entities/Polynomial.java
@@ -11,6 +11,7 @@ public class Polynomial {
 
     public Polynomial(RecurrentRelation recurrentRelation) {
         this.recurrentRelation = recurrentRelation;
+        modF = recurrentRelation.getModF();
         reverseCoefficients(recurrentRelation.getCoefficients());
     }
 
@@ -44,7 +45,7 @@ public class Polynomial {
     }
 
     private void reverseCoefficients(List<Integer> coefficients) {
-        setCoefficients(new ArrayList<>());
+        this.coefficients = new ArrayList<>();
         coefficients.forEach(coefficient -> this.coefficients.add(coefficient * -1));
         this.coefficients.add(1);
     }
@@ -52,15 +53,35 @@ public class Polynomial {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for (int i = coefficients.size() - 1; i >= 0; i--) {
+        if (coefficients.get(coefficients.size() - 1) == 1 || coefficients.get(coefficients.size() - 1) == -1) {
+            result.append("X^").append(coefficients.size() - 1);
+        } else {
+            result.append(coefficients.get(coefficients.size() - 1)).append("X^").append(coefficients.size() - 1);
+        }
+        for (int i = coefficients.size() - 2; i >= 0; i--) {
             if (coefficients.get(i) != 0) {
-                if (coefficients.get(i) != 1 && coefficients.get(i) != -1) {
-                    result.append(coefficients.get(i)).append("X^").append(i);
-                } else {
-                    result.append("X^").append(i);
-                }
-                if (i != 1) {
+                if (coefficients.get(i) > 0) {
                     result.append(" + ");
+                    if (i == 0) {
+                        result.append(1);
+                    } else {
+                        if (coefficients.get(i) == 1 || coefficients.get(i) == -1) {
+                            result.append("X^").append(i);
+                        } else {
+                            result.append(coefficients.get(i)).append("X^").append(i);
+                        }
+                    }
+                } else {
+                    result.append(" - ");
+                    if (i == 0) {
+                        result.append(1);
+                    } else {
+                        if (coefficients.get(i) == 1 || coefficients.get(i) == -1) {
+                            result.append("X^").append(i);
+                        } else {
+                            result.append(coefficients.get(i) * -1).append("X^").append(i);
+                        }
+                    }
                 }
             }
         }
